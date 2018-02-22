8af4fa8f54900cd9d509dc7eca73fcff9904039b
Tyler <tyler.green@reeher.com>
2018-02-21 21:03:59 -0600

    implemented HTML for Do's just started JS

---
+++ js/toDoList.js
@@ -0,0 +1,15 @@
+var doRowObject;
+
+window.onload = function() {
+
+}
+
+function addDoRow() {
+    var description = document.getElementById("descriptionInput");
+    var date = document.getElementById("dateInput");
+    var categoryDropDown = document.getElementById("categoryInput");
+    var categoryText = document.getElementById("categoryTextInput");
+    var categoryColor = document.getElementById("colorSelect");
+    var status = document.getElementById("completedInput");
+}
+
