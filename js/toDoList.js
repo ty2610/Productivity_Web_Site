71c88a3e9bf4cb19a275e9cf87227548ba8640d1
Tyler Green <gree0209@stthomas.edu>
2018-02-25 21:49:16 -0600

    fleshed out much of the ToDo
    now handle checks correctly, and colors/custom categories. Created sorting for the category name. Need to implement sorting for the date created and if the completed box is selected.
    
    Still have to look into canvas's and cookies.

--- js/toDoList.js
+++ js/toDoList.js
@@ -26,6 +26,7 @@ function addDoRow() {
     var categoryText = document.getElementById("categoryTextInput");
     var categoryColor = document.getElementById("colorSelect");
     var status = document.getElementById("completedInput");
+    var timeCreated = new Date();
 
     if(description.value === "") {
         return "You must input a description for the do";
@@ -36,12 +37,13 @@ function addDoRow() {
     } else if(categoryDropDown.value !== "Pick a category" && categoryText.value !== "") {
         return "You can either pick an existing category, or make your own, not both.";
     }
-    if(categoryDropDown.value !== ""){
+    if(categoryDropDown.value !== "Pick a category"){
         var catColor = getColorForName(categoryDropDown.value);
-        doRowObject.push({id:rowUniqueCount, description:description.value, date: date.value, category:categoryDropDown.value, color:catColor, status:status.checked});
+        doRowObject.push({id:rowUniqueCount, description:description.value, date: date.value, category:categoryDropDown.value, color:catColor, status:status.checked, timeCreated:timeCreated});
     } else {
-        categoryObject += {name:categoryText.value, color:categoryColor.value};
-        doRowObject.push({id:rowUniqueCount, description:description.value, date: date.value, category:categoryText.value, color:categoryColor.value, status:status.checked});
+        categoryObject.push({name:categoryText.value, color:categoryColor.value});
+        doRowObject.push({id:rowUniqueCount, description:description.value, date: date.value, category:categoryText.value, color:categoryColor.value, status:status.checked, timeCreated:timeCreated});
+        addCategoryToSelect(categoryText.value);
     }
     rowUniqueCount++;
     return "success";
@@ -67,23 +69,29 @@ function toDoReDraw() {
     var newCell1;
     var newCell2;
     var newCell3;
-    var newCell4;
+    var newcell4;
+    var newCell5;
 
     for(var i=0; i<doRowObject.length; i++) {
         row = table.insertRow(table.rows.length);
+        row.id = "insertedRow" + doRowObject[i].id;
+
         newCell0 = row.insertCell(0);
         newCell1 = row.insertCell(1);
         newCell2 = row.insertCell(2);
         newCell3 = row.insertCell(3);
-        newCell4 = row.insertCell(4);
+        newcell4 = row.insertCell(4);
+        newCell5 = row.insertCell(5);
 
         newCell0.innerHTML = "<td>" + doRowObject[i].description + "</td>";
         newCell1.innerHTML = "<td>" + doRowObject[i].date + "</td>";
         newCell2.innerHTML = "<td>" + doRowObject[i].category + "</td>";
-        newCell3.innerHTML = "<td>" + "<input type=\"checkbox\" onclick=\"checkBoxClick(" + doRowObject[i].id + ")\">" + "</td>";
-        newCell4.innerHTML = "<td>" + "<button type=\"button\" onclick=\"doDelete(" + doRowObject[i].id + ")\">Delete do</button>" + "</td>";
+        newCell3.innerHTML = "<td>" + "<input id=\"checkBox" + doRowObject[i].id + "\" type=\"checkbox\" onclick=\"checkBoxClick(" + doRowObject[i].id + ")\">" + "</td>";
+        newcell4.innerHTML = "<td>" + doRowObject[i].timeCreated + "</td>";
+        newCell5.innerHTML = "<td>" + "<button type=\"button\" onclick=\"doDelete(" + doRowObject[i].id + ")\">Delete do</button>" + "</td>";
     }
-
+    doCheckBoxAction();
+    colorRows();
 }
 
 function doDelete(id) {
@@ -91,3 +99,39 @@ function doDelete(id) {
     toDoReDraw();
 }
 
+function doCheckBoxAction() {
+    for(var i=0; i<doRowObject.length; i++) {
+        if(doRowObject[i].status) {
+            document.getElementById("checkBox"+doRowObject[i].id).checked = true;
+        }
+    }
+}
+
+function checkBoxClick(id) {
+    for(var i=0; i<doRowObject.length; i++) {
+        if(doRowObject[i].id === id) {
+            doRowObject[i].status = !doRowObject[i].status;
+        }
+    }
+}
+
+function colorRows() {
+    for(var i=0; i<doRowObject.length; i++) {
+        var row = document.getElementById("insertedRow"+doRowObject[i].id);
+        row.style.backgroundColor = doRowObject[i].color;
+    }
+}
+
+function addCategoryToSelect(name) {
+    var multiSelect = document.getElementById("categoryInput");
+    var option = document.createElement("option");
+    option.text = name;
+    multiSelect.add(option);
+}
+
+function sortCategory() {
+    doRowObject.sort( function(a,b) {
+        return a.category.localeCompare(b.category);
+    });
+    toDoReDraw();
+}
